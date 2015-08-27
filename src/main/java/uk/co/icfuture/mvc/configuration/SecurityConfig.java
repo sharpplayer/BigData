b3289ffbb39c97d6ec5b38cb03508abada25137c
sharpplayer <comkraft@pavilion.co.uk>
2015-08-27 15:45:32 +0100

    Example filtering of security on HttpMethod

--- src/main/java/uk/co/icfuture/mvc/configuration/SecurityConfig.java
+++ src/main/java/uk/co/icfuture/mvc/configuration/SecurityConfig.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -35,6 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/", "/login").permitAll()
+				.antMatchers(HttpMethod.GET, "/question/**").permitAll()
 				.antMatchers("/admin/**").hasRole("ADMIN").anyRequest()
 				.authenticated().and().formLogin().loginPage("/login")
 				.loginProcessingUrl("/login").defaultSuccessUrl("/")
